a3af4403baf5376d89656ffbf6c57c7028582f95
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2019-09-19 17:01:41 +0100

    Find owner added

--- pet-clinic-data/src/main/java/pt/home/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/pt/home/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -9,6 +9,7 @@ import pt.home.sfgpetclinic.repositories.PetTypeRepository;
 import pt.home.sfgpetclinic.services.OwnerService;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -28,6 +29,11 @@ public class OwnerSDJpaService implements OwnerService {
     @Override
     public Owner findByLastName(String lastName) {
         return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLikeIgnoreCase(String lastName) {
+        return ownerRepository.findAllByLastNameLikeIgnoreCase(lastName);
     }
 
     @Override
